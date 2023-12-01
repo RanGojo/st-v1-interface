96036ce4da853b37f4d20df03afe77b76f36b8cf
Arthur Ribeiro <arthurmr13@hotmail.com>
2023-11-30 22:45:04 -0300

    feat: set chain config via reactive var

--- src/hooks/useActiveChain.tsx
+++ src/hooks/useActiveChain.tsx
@@ -1,21 +1,21 @@
-import { useCallback, useState } from "react";
+import { useEffect, useState } from "react";
 import { Network } from "@/types/Network";
-import chainConfig, { ChainConfig } from "@/config/chain";
+import chainConfig, { ChainConfig, selectedChainIdVar } from "@/config/chain";
+import { useReactiveVar } from "@apollo/client";
 
 export default function useActiveChain() {
-  const [chainId, setChainId] = useState<number>(Network.Mainnet)
+  const chainId = useReactiveVar(selectedChainIdVar)
   const [config, setConfig] = useState<ChainConfig>(chainConfig(chainId))
   const [isTestnet, setIsTestnet] = useState<boolean>(true)
-  const setActiveChain = useCallback((chainId: number) => {
-    setChainId(chainId)
-    setIsTestnet(chainId !== Network.Mainnet)
+
+  useEffect(() => {
     setConfig(chainConfig(chainId))
-  }, [])
+    setIsTestnet(chainId !== Network.Mainnet)
+  }, [chainId]);
 
   return {
     chainId,
     config,
     isTestnet,
-    setActiveChain
   }
 }
